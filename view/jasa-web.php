b32a3f112308e6b0a4a4f13bc700620c109f6291
dion-zebua <zebuadbless@gmail.com>
2026-09-04 14:09:55 +0700

    malas ngoding, paham 🖐

--- view/jasa-web.php
+++ view/jasa-web.php
@@ -12,42 +12,53 @@ $meta = [
     "img" => "$domain/img/jasa-web.jpeg",
 ];
 
-$apiUrl = "https://script.google.com/macros/s/AKfycbwbPMymcZ7SlRgEjTy6Z_QHbxps_hSuLUyNOhkmBp5ARBv2WiW_w4U6XvdgCaVhdqfq/exec";
+if (isset($_GET['hit'])) {
+    $apiUrl = "https://script.google.com/macros/s/AKfycbwbPMymcZ7SlRgEjTy6Z_QHbxps_hSuLUyNOhkmBp5ARBv2WiW_w4U6XvdgCaVhdqfq/exec";
 
-try {
-    $ch = curl_init($apiUrl);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');
+    try {
+        $ch = curl_init($apiUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');
 
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
-    $response = curl_exec($ch);
-    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    $curlError = curl_error($ch);
+        $response = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $curlError = curl_error($ch);
 
-    curl_close($ch);
+        curl_close($ch);
 
-    if ($response === false || !empty($curlError)) {
-        throw new Exception("cURL Error: " . $curlError);
-    }
+        if ($response === false || !empty($curlError)) {
+            throw new Exception("cURL Error: " . $curlError);
+        }
 
-    if ($httpCode !== 200) {
-        throw new Exception("API Error: Server mengembalikan HTTP Code " . $httpCode);
-    }
-
-    $data = json_decode($response, true);
-
-    if (json_last_error() !== JSON_ERROR_NONE) {
-        throw new Exception("Gagal melakukan decode JSON.");
-    }
-
-    if (isset($data["values"])) {
+        if ($httpCode !== 200) {
+            throw new Exception("API Error: Server mengembalikan HTTP Code " . $httpCode);
+        }
+        $data = json_decode($response, true);
         unset($data["values"][0]);
+
+        file_put_contents(
+            ABSPATH . '\rute.json',
+            json_encode($data["values"], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)
+        );
+    } catch (Exception $e) {
+        file_put_contents(
+            ABSPATH . '\rute.json',
+            json_encode([], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)
+        );
     }
-} catch (Exception $e) {
-    $data["values"][0] = "Terjadi kesalahan: " . $e->getMessage();
 }
+
+$web = json_decode(
+    file_get_contents(ABSPATH . "\\rute.json"),
+    true
+);
+
+$web = is_array($web) ? $web : [];
+
+
 
 require_once ABSPATH . '/layout/head.php';
 
@@ -125,18 +136,18 @@ require_once ABSPATH . '/layout/head.php';
                 <div class="h-6"></div>
                 <p class="text-xl text-gray-400 md:pr-10">
                     Berpengalaman membangun lebih dari
-                    <span id="jumlahWeb"><?= ($httpCode != 200) ? 195 : (count($data["values"]) - 1) ?></span>+ sistem digital, mulai dari landing page perusahaan, travel, toko online,
+                    <span id="jumlahWeb"><?= count($web) == 0 ? 195 : count($web) ?></span>+ sistem digital, mulai dari landing page perusahaan, travel, toko online,
                     hingga dashboard manajemen umrah.
                 </p>
                 <div class="h-8"></div>
 
             </div>
             <ul id="domain-list" class="rounded-lg bg-gradient-to-br from-gray-900 to-black grid sm:grid-cols-2 lg:grid-cols-3 gap-4 p-5 border-t border-gray-800 md:col-span-full">
-                <?php if ($httpCode != 200): ?>
-                    <li class="text-sm md:text-base"><?= $data["values"][0] ?></li>
+                <?php if (count($web) < 1): ?>
+                    <li class="text-sm md:text-base">Data tidak ditemukan</li>
 
-                <?php else: ?>
-                    <?php foreach ($data["values"] as $key => $value) : ?>
+                <?php else : ?>
+                    <?php foreach ($web as $key => $value) : ?>
                         <li class="flex items-center gap-2 text-sm md:text-base">
                             <span class="text-blue-500">•</span>
                             <a class="hover:underline" href="https://<?= str_replace(' ', '?', $value) ?>" target="_blank">
